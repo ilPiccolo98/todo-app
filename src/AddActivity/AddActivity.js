f5acf3816f1607381017fdea07058e7577936743
Andrea Briganti <andrea.briganti@nttdata.com>
2021-10-13 17:36:12 +0200

    fix tests

--- src/AddActivity/AddActivity.js
+++ src/AddActivity/AddActivity.js
@@ -62,7 +62,7 @@ const AddActivity = (props) => {
           data-testid="name-field-add-activity"
           id="name"
           name="name"
-          placeholder="Name"
+          placeholder="errore"
           onChange={handleChangeNameField}
           value={name}
         />
